a9d86038afb215fe0600835d103998c0c7d7accd
nagalaxmisl <nagalaxmislbhat@gmail.com>
2018-04-14 07:23:52 +0000

    added FeedService

--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -11,5 +11,23 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
+    public FeedResponse getFeedByService(String query) {
+        FeedResponse feedResponse = new FeedResponse();
+        try {
+            WSRequest feedRequest = WS.url("https://news.google.com/news");
+            CompletionStage<WSResponse> responsePromise = feedRequest
+                    .setQueryParameter("q", query)
+                    .setQueryParameter("output", "rss")
+                    .get();
+            Document response = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+            Node item=response.getFirstChild().getFirstChild().getChildNodes().item(10);
+            feedResponse.title=item.getChildNodes().item(0).getFirstChild().getNodeValue();
+            feedResponse.description=item.getChildNodes().item(4).getFirstChild().getNodeValue();
+            feedResponse.pubDate=item.getChildNodes().item(3).getFirstChild().getNodeValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return feedResponse;
 
+    }
 }
