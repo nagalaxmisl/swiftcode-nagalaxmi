e2430c132c2b7e35cb017327a555d40e184da6ad
nagalaxmisl <nagalaxmislbhat@gmail.com>
2018-04-15 11:48:53 +0000

    actor3

--- app/actors/MessageActor.java
+++ app/actors/MessageActor.java
@@ -45,7 +45,7 @@ public class MessageActor extends UntypedActor {
             messageObject.sender = Message.Sender.USER;
             out.tell(objectMapper.writeValueAsString(messageObject), self());
             newsAgentService.getNewsAgentResponse(messageObject.text, UUID.randomUUID());
-            String query = newsAgentService.getNewsAgentResponse("Find" + message, UUID.randomUUID()).query;
+            String query = newsAgentService.getNewsAgentResponse("Find " + message, UUID.randomUUID()).query;
             FeedResponse feedResponse = feedService.getFeedByService(query);
             messageObject.text = (feedResponse.title == null) ? "No results found" : "Showing results for: " + query;
             messageObject.feedResponse = feedResponse;
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,20 +1,26 @@
 var app = angular.module('chatApp', ['ngMaterial']);
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('purple')
+        .accentPalette('green');
+});
 
-app.controller('chatController', function ($scope) {
-    $scope.messages = [
-        {
-            'sender': 'USER',
-            'text': 'hello'
-    	},
-        {
-            'sender': 'BOT',
-            'text': 'hello what are you doing'
-    	},
-        {
-            'sender': 'USER',
-            'text': 'sleeping'
-    	}
-    ];
+app.controller('chatController', function ($scope, $sce) {
+    $scope.messages = [];
+    $scope.trust = $sce.trustAsHtml;
+    var exampleSocket = new WebSocket('ws://localhost:9000/chatSocket');
+    exampleSocket.onmessage = function (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+    };
+    $scope.sendMessage = function () {
+        exampleSocket.send($scope.userMessage);
+        $scope.userMessage = '';
+    };
 
 
 });
